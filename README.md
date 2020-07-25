73c90807263ecb94f62206ce38486b3962f3b37e
Supphachoke Suntiwichaya <mrchoke@gmail.com>
2020-07-25 18:44:47 +0700

    T-Face API JacaScript DEMO init

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ai4thai-tface-javascript-demo
+
 DEMO สำหรับการใช้งาน AI For Thai T-Face API โดยใช้ Javascript
+
+## ตัวอย่าง
+
+- Form Post โดยใช้ Axios
+- Form Post โดยใช้ Fetch
+- JSON Base64 โดยใช้ Axios
+- JSON Base64 โดยใช้ Fetch
+
+## Config
+
+t-face.js
+
+```javascript
+const project_id = "";
+const api_key = "";
+```
+
+[ศึกษาเพิ่มเติม](https://aiforthai.in.th)
---
+++ t-face-base64-fetch.js
@@ -0,0 +1,69 @@
+/*
+    AI For Thai T-Face API
+    JavaScript Demo
+    https://aiforthai.in.th
+    https://www.facebook.com/groups/aiforthai
+
+    NECTEC NSTDA
+    25/07/2020
+
+*/
+const url = "https://api.aiforthai.in.th/t-face/base64/" + project_id;
+
+// ค้นหาด้วย fetch
+const searchFetch = async (b64) => {
+  const res = await fetch(url, {
+    method: "POST",
+
+    body: JSON.stringify({ image: b64 }),
+    headers: {
+      "Content-Type": "application/json",
+      Apikey: api_key,
+    },
+  });
+  return res.json();
+};
+
+// ประมวลผลการค้นหา
+const getResult = async (b64) => {
+  resultText.innerText = "กำลังค้นหา....";
+
+  const res = await searchFetch(b64);
+
+  if (res && res.boxes) {
+    preview.src = drawResult(res.boxes);
+    resultText.innerText = "พบ " + res.boxes.length + "ใบหน้า";
+    if (res.result && res.result.length) {
+      result.innerHTML = showResult(res.result, res.boxes);
+    } else {
+      result.innerHTML = "ใบหน้าไม่ตรงกับฐานข้อมูล";
+    }
+  } else {
+    resultText.innerText = "ไม่เจอใบหน้าบนรูปนี้";
+  }
+};
+
+// event on change เมื่อเลือกรูป
+img.onchange = () => {
+  const files = img.files[0] || img.files;
+  loadImage.parseMetaData(files, (data) => {
+    const options = {
+      canvas: true,
+      maxHeight: 640,
+      maxWidth: 640,
+    };
+    if (data.exif) {
+      options.orientation = data.exif.get("Orientation");
+    }
+    loadImage(
+      files,
+      (canvas) => {
+        preview.src = orig_img = canvas.toDataURL("image/jpeg");
+        createCanvas();
+        const b64 = canvas.toDataURL("image/jpeg").split(",")[1];
+        getResult(b64);
+      },
+      options
+    );
+  });
+};
